4a853d1c7cfff050ac822afd89bc87e47fccdc4f
NvRSleep <111803646+NvRSleep@users.noreply.github.com>
2024-11-12 11:45:32 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+// Data GeoJSON dalam format JavaScript
+var geojsonData = {
+    "type": "FeatureCollection",
+    "name": "Gedung_ITERA",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [
+        { "type": "Feature", "properties": { "OID_": 0, "Name": "Gedung GKU 1" }, "geometry": { "type": "Point", "coordinates": [ 105.310394, -5.360931 ] } },
+        { "type": "Feature", "properties": { "OID_": 1, "Name": "Gedung A" }, "geometry": { "type": "Point", "coordinates": [ 105.314404, -5.357899 ] } },
+        { "type": "Feature", "properties": { "OID_": 2, "Name": "Gedung B" }, "geometry": { "type": "Point", "coordinates": [ 105.315336, -5.357887 ] } },
+        { "type": "Feature", "properties": { "OID_": 3, "Name": "Gedung C" }, "geometry": { "type": "Point", "coordinates": [ 105.313565, -5.358400 ] } },
+        { "type": "Feature", "properties": { "OID_": 4, "Name": "Gedung D" }, "geometry": { "type": "Point", "coordinates": [ 105.313516, -5.358744 ] } },
+        { "type": "Feature", "properties": { "OID_": 5, "Name": "Gedung E" }, "geometry": { "type": "Point", "coordinates": [ 105.315538, -5.360009 ] } },
+        { "type": "Feature", "properties": { "OID_": 6, "Name": "Gedung F" }, "geometry": { "type": "Point", "coordinates": [ 105.313751, -5.361391 ] } },
+        { "type": "Feature", "properties": { "OID_": 7, "Name": "Masjid Baitul Ilmi ITERA" }, "geometry": { "type": "Point", "coordinates": [ 105.312633, -5.359061 ] } },
+        { "type": "Feature", "properties": { "OID_": 8, "Name": "Gedung Labtek 1" }, "geometry": { "type": "Point", "coordinates": [ 105.310209, -5.360300 ] } },
+        { "type": "Feature", "properties": { "OID_": 9, "Name": "Gedung Labtek 3" }, "geometry": { "type": "Point", "coordinates": [ 105.309103, -5.360301 ] } },
+        { "type": "Feature", "properties": { "OID_": 10, "Name": "Gedung Labtek OZT" }, "geometry": { "type": "Point", "coordinates": [ 105.310453, -5.361512 ] } },
+        { "type": "Feature", "properties": { "OID_": 11, "Name": "Gedung Asrama 1" }, "geometry": { "type": "Point", "coordinates": [ 105.317435, -5.358440 ] } },
+        { "type": "Feature", "properties": { "OID_": 12, "Name": "Gedung Asrama 2" }, "geometry": { "type": "Point", "coordinates": [ 105.319431, -5.358475 ] } },
+        { "type": "Feature", "properties": { "OID_": 13, "Name": "Gedung Asrama 3" }, "geometry": { "type": "Point", "coordinates": [ 105.319033, -5.359153 ] } },
+        { "type": "Feature", "properties": { "OID_": 14, "Name": "Gedung Asrama 4" }, "geometry": { "type": "Point", "coordinates": [ 105.317444, -5.359153 ] } },
+        { "type": "Feature", "properties": { "OID_": 15, "Name": "Gedung Asrama 5" }, "geometry": { "type": "Point", "coordinates": [ 105.318478, -5.358445 ] } },
+        { "type": "Feature", "properties": { "OID_": 16, "Name": "Kantin BKL" }, "geometry": { "type": "Point", "coordinates": [ 105.316506, -5.357602 ] } },
+        {
+            "type": "Feature",
+            "properties": { "OID_": 17, "Name": "Polygon Gedung F", "color": "red", "fillColor": "#ff0000", "fillOpacity": 0.5 },
+            "geometry": { "type": "Polygon", "coordinates": [[ [ 105.315831, -5.359799 ], [ 105.315831, -5.359957 ], [ 105.315571, -5.359957 ], [ 105.315571, -5.360474 ], [ 105.315401, -5.360474 ], [ 105.315401, -5.359635 ], [ 105.315571, -5.359635 ], [ 105.315571, -5.359799 ] ]] }
+        }
+    ]
+};
